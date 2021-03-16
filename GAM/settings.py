18d6035c69a769560b43cc751f248fc496d85f24
KirandeepKaur03 <kkirandeep2@gmail.com>
2021-03-16 16:38:19 +0530

    edit

--- GAM/settings.py
+++ GAM/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^ls%yt-&kbmeab91p=(t+tl6p^fmnth%&^bqfkwp@8)j+m)89i%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
